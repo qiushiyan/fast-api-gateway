4838eafdc69779e944bb9e187159059075426bd3
qiushi <qiushi.yann@gmail.com>
2021-11-14 21:22:38 -0600

    update test

--- tests/test_core.py
+++ tests/test_core.py
@@ -7,4 +7,4 @@ client = TestClient(app)
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "hello world"}
+    assert response.json() == {"message": "hello world from fastapi"}
